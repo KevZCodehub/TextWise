b52cfc77587a67549f28218e28e71a3b5d750fcc
qinghanz <qinghan.zhang@mail.mcgill.ca>
2023-06-25 14:22:11 -0400

    initial backend service

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello from the backend!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    text = data['text']
+
+    # Perform predictive text completion using your C implementation
+
+    # Dummy example: Appending ' world' to the input text
+    predicted_text = text + ' world'
+
+    return jsonify({'predictedText': predicted_text})
+
+if __name__ == '__main__':
+    print("running!")
+    app.run(port=8000)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [predictedText, setPredictedText] = useState("");
+  const [backendStatus, setBackendStatus] = useState("Connecting...");
+
+  useEffect(() => {
+    // Check backend connection status
+    fetch("/predict")
+      .then(() => setBackendStatus("Connected"))
+      .catch(() => setBackendStatus("Disconnected"));
+  }, []);
+
+  const handleInputChange = (event) => {
+    const newText = event.target.value;
+    setText(newText);
+
+    // Send text to the C service
+    fetch("/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newText }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPredictedText(data.predictedText);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div>
+      <h1>Predictive Text App</h1>
+      <div>
+        <strong>Backend Status:</strong> {backendStatus}
+      </div>
+      <textarea
+        value={text}
+        onChange={handleInputChange}
+        rows="5"
+        cols="50"
+      ></textarea>
+      <br />
+      <div>
+        <h2>Predicted Text:</h2>
+        <p>{predictedText}</p>
+      </div>
     </div>
   );
 }
