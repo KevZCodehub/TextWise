c16dc4fa9b644e3d3b25523774d33422143b53a5
ThaLoudy <134550320+ThaLoudy@users.noreply.github.com>
2023-07-21 13:28:41 -0400

    added feature to cut of sentences.

--- app.py
+++ app.py
@@ -27,8 +27,28 @@ def predict():
     # Decode and format the predicted text
     predicted_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
+    # Cut off the text at the next occurrence of ". ", "? ", or "! "
+    next_sentence_index = find_next_sentence_index(predicted_text)
+
+    # Determine the final cutoff index
+    if next_sentence_index != -1:
+        cutoff_index = next_sentence_index + 2
+    else:
+        cutoff_index = len(predicted_text)
+
+    # Cut off the text at the determined index
+    predicted_text = predicted_text[:cutoff_index]
+
     return jsonify({'predictedText': predicted_text})
 
+def find_next_sentence_index(text):
+    sentence_endings = [". ", "? ", "! "]
+    min_index = -1
+    for ending in sentence_endings:
+        index = text.find(ending)
+        if index != -1 and (min_index == -1 or index < min_index):
+            min_index = index
+    return min_index
 if __name__ == '__main__':
     print("running!")
     app.run(port=8000)
